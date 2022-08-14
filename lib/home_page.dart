ce5741bb005bd30aae7066c1f323392691fd9f1b
arpitparekh <arpitparekh9@gmail.com>
2022-08-14 20:10:56 +0530

    complete application habit tracker only this remaining is database storage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/list_tiles.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,27 +13,213 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List habitList = [
+    ['Exercise', false, 0, 1],
+    ['Reading', false, 0, 1],
+    ['Meditation', false, 0, 1],
+    ['Coding', false, 0, 1],
+    ['Playing Guitar', false, 0, 1],
+  ];
+
+  int selectedValue = 0;
+  late SharedPreferences preferences;
+  TextEditingController habitControl = TextEditingController();
+
+  void habitOnClick(int index) {
+    var startTime = DateTime.now();
+    var elapsedTime = habitList[index][2];
+
+    setState(() {
+      habitList[index][1] = !habitList[index][1];
+    });
+
+    if (habitList[index][1]) {
+      Timer.periodic(const Duration(seconds: 1), (timer) {
+        setState(() {
+          if (!habitList[index][1]) {
+            timer.cancel();
+          }
+          var currentTime = DateTime.now();
+          habitList[index][2] = elapsedTime +
+              currentTime.second -
+              startTime.second +
+              60 * (currentTime.minute - startTime.minute) +
+              3600 * (currentTime.hour - startTime.hour);
+        });
+      });
+    }
+  }
+
+  void timerOnClick(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: SizedBox(
+            height: 400,
+            width: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Set Time for',
+                  style: TextStyle(fontSize: 25),
+                ),
+                Text(
+                  '${habitList[index][0]}',
+                  style: const TextStyle(fontSize: 25),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CupertinoTimerPicker(
+                  mode: CupertinoTimerPickerMode.hm,
+                  onTimerDurationChanged: (value) {
+                    setState(() {
+                      habitList[index][3] = value.inMinutes;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                  child: const Text('Done'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initialize();
+  }
+
+  Future initialize() async {
+    preferences = await SharedPreferences.getInstance();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
+        actions: [
+          GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: SizedBox(
+                      height: 220,
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Add Good Habit',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: habitControl,
+                                decoration: const InputDecoration(
+                                    hintText: 'Enter Habit Name',
+                                    labelText: 'Enter Habit Name',
+                                    border: OutlineInputBorder()),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextButton(
+                              child: const Text('Done'),
+                              onPressed: () {
+                                setState(() {
+                                  habitList
+                                      .add([habitControl.text, false, 0, 1]);
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.add),
+            ),
+          ),
+        ],
         backgroundColor: Colors.grey[900],
         title: const Text('Consistency is Key'),
         centerTitle: false,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 10,
-            ),
-            HabitTile(),
-            HabitTile(),
-            HabitTile(),
-            HabitTile(),
-            HabitTile(),
-          ],
+      body: ListView.builder(
+        itemCount: habitList.length,
+        itemBuilder: (context, index) => HabitTile(
+          habitName: habitList[index][0],
+          currentTime: habitList[index][2],
+          goalTime: habitList[index][3],
+          onDeleteTap: () {
+            setState(() {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('Do you Really Want To Delete'),
+                    actions: [
+                      TextButton(
+                        child: const Text('Cancel'),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      TextButton(
+                        child: const Text('Delete'),
+                        onPressed: () {
+                          setState(() {
+                            if (habitControl.text != '') {
+                              habitList.removeAt(index);
+                            }
+                            Navigator.pop(context);
+                          });
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            });
+          },
+          onPlayTap: () {
+            habitOnClick(index);
+          },
+          onTimeTap: () {
+            timerOnClick(index);
+          },
+          habitStarted: habitList[index][1],
         ),
       ),
     );
--- lib/list_tiles.dart
+++ lib/list_tiles.dart
@@ -2,7 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class HabitTile extends StatelessWidget {
-  const HabitTile({Key? key}) : super(key: key);
+  final String habitName;
+  final VoidCallback onPlayTap;
+  final VoidCallback onTimeTap;
+  final VoidCallback onDeleteTap;
+  final int currentTime;
+  final int goalTime;
+  final bool habitStarted;
+
+  const HabitTile(
+      {Key? key,
+      required this.habitName,
+      required this.onPlayTap,
+      required this.onTimeTap,
+      required this.currentTime,
+      required this.goalTime,
+      required this.habitStarted,
+      required this.onDeleteTap})
+      : super(key: key);
+
+  String convertSec(int seconds) {
+    String secs = (seconds % 60).toString();
+    String mins = (seconds / 60).toStringAsFixed(5);
+
+    if (secs.length == 1) {
+      secs = '0$secs';
+    }
+
+    if (mins[1] == '.') {
+      mins = mins.substring(0, 1);
+    }
+
+    return '$mins : $secs';
+  }
+
+  double percentageCount() {
+    return (currentTime / (goalTime * 60));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,27 +55,71 @@ class HabitTile extends StatelessWidget {
           children: [
             Row(
               children: [
-                CircularPercentIndicator(
-                  radius: 25,
-                  percent: 0.7,
+                GestureDetector(
+                  onTap: onPlayTap,
+                  child: SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: Stack(
+                      children: [
+                        Center(
+                            child: habitStarted
+                                ? const Icon(Icons.pause)
+                                : const Icon(Icons.play_arrow)),
+                        Center(
+                          child: CircularPercentIndicator(
+                            radius: 30,
+                            percent:
+                                percentageCount() < 1 ? percentageCount() : 1,
+                            progressColor: percentageCount() > 0.5
+                                ? (percentageCount() > 0.75
+                                    ? Colors.green
+                                    : Colors.orange)
+                                : Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
                 const SizedBox(
-                  width: 10,
+                  width: 20,
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
+                  children: [
                     Text(
-                      'Exercise',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                      habitName,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20),
                     ),
-                    Text('2:00 / 10:00 = 20%')
+                    Text(
+                        '${convertSec(currentTime)} / $goalTime = ${(percentageCount() * 100).toStringAsFixed(0)} %'),
                   ],
                 ),
               ],
             ),
-            const Icon(Icons.timer),
+            Column(
+              children: [
+                GestureDetector(
+                  onTap: onTimeTap,
+                  child: const Icon(
+                    Icons.timer,
+                    color: Colors.green,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                GestureDetector(
+                  onTap: onDeleteTap,
+                  child: const Icon(
+                    Icons.restore_from_trash_sharp,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
